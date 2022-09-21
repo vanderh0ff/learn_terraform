4e1ab4a3dc81d09bb042467e9f2483bbcff2367c
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-09-21 00:15:55 -0400

    provider locking

---
+++ state/provider_versioning/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"
+    }
+  }
+
+  required_version = ">= 1.1"
+}
