9f4fb9dabbffccd8390f3d474463c47e0503fcfe
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-09-21 22:28:27 -0400

    fix outputs for each

--- troubleshooting/outputs.tf
+++ troubleshooting/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.web_app.id
+  value       = [for instance in aws_instance.web_app: instance.id]
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web_app.public_ip
+  value       = [for instance in aws_instance.web_app: instance.public_ip]
 }
 
 output "instance_name" {
   description = "Tags of the EC2 instance"
-  value       = aws_instance.web_app.tags
+  value       = [for instance in aws_instance.web_app: instance.tags]
 }
