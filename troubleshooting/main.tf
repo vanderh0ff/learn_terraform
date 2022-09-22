c5cc33de544155afb6747d838120443f80158f1e
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-09-21 22:24:13 -0400

    correct for_each error

--- troubleshooting/main.tf
+++ troubleshooting/main.tf
@@ -33,10 +33,10 @@ data "http" "myip" {
 }
 
 resource "aws_instance" "web_app" {
-  for_each               = aws_security_group.*.id
+  for_each               = local.security_groups
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [each.id]
+  vpc_security_group_ids = [each.value]
   user_data              = <<-EOF
               #!/bin/bash
               apt-get update
@@ -46,30 +46,18 @@ resource "aws_instance" "web_app" {
               systemctl restart apache2
               EOF
   tags = {
-    Name = "${var.name}-learn"
+    Name = "${var.name}-learn-${each.key}"
   }
 }
 
 resource "aws_security_group" "sg_ping" {
   name = "Allow Ping"
 
-  ingress {
-    from_port       = -1
-    to_port         = -1
-    protocol        = "icmp"
-    security_groups = [aws_security_group.sg_8080.id]
-  }
 }
 
 resource "aws_security_group" "sg_8080" {
   name = "Allow 8080"
 
-  ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_ping.id]
-  }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
@@ -97,3 +85,27 @@ resource "aws_security_group_rule" "allow_localhost_ping" {
   security_group_id = aws_security_group.sg_ping.id
 }
 
+resource "aws_security_group_rule" "allow_ping" {
+  type                     = "ingress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = "icmp"
+  security_group_id        = aws_security_group.sg_ping.id
+  source_security_group_id = aws_security_group.sg_8080.id
+}
+
+resource "aws_security_group_rule" "allow_8080" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg_8080.id
+  source_security_group_id = aws_security_group.sg_ping.id
+}
+
+locals {
+  security_groups = {
+    sg_ping = aws_security_group.sg_ping.id,
+    sg_8080 = aws_security_group.sg_8080.id,
+  }
+}
