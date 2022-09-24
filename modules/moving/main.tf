49e5b7480c72b7d84b9ab7afc6bc02139717a424
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-09-23 22:44:36 -0400

    moved resources

--- modules/moving/main.tf
+++ modules/moving/main.tf
@@ -64,3 +64,23 @@ module "ec2_instance" {
   security_group = module.web_security_group.security_group_id
   public_subnets = module.vpc.public_subnets
 }
+
+moved {
+  from = module.security_group.aws_security_group.sg_8080
+  to   = module.web_security_group.aws_security_group.this[0]
+}
+
+moved {
+  from = module.security_group.aws_security_group_rule.ingress_rule
+  to   = module.web_security_group.aws_security_group_rule.ingress_with_cidr_blocks[0]
+}
+
+moved {
+  from = module.security_group.aws_security_group_rule.egress_rule
+  to   = module.web_security_group.aws_security_group_rule.egress_with_cidr_blocks[0]
+}
+
+moved {
+  from = aws_instance.example
+  to = module.ec2_instance.aws_instance.example
+}
