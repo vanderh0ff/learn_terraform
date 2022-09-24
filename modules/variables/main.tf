540f5dfbfccf3fd594ff97722b981362cd988c3b
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-09-24 00:02:09 -0400

    abstract out subnets

--- modules/variables/main.tf
+++ modules/variables/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = var.aws_region
 }
 
 data "aws_availability_zones" "available" {
@@ -18,19 +18,16 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.64.0"
 
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr_block
 
   azs             = data.aws_availability_zones.available.names
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
 
   enable_nat_gateway = true
-  enable_vpn_gateway = false
+  enable_vpn_gateway = var.enable_vpn_gateway
 
-  tags = {
-    project     = "project-alpha",
-    environment = "dev"
-  }
+  tags = var.resource_tags
 }
 
 module "app_security_group" {
@@ -109,7 +106,7 @@ module "elb_http" {
 module "ec2_instances" {
   source = "./modules/aws-instance"
 
-  instance_count     = 2
+  instance_count     = var.instance_count
   instance_type      = "t2.micro"
   subnet_ids         = module.vpc.private_subnets[*]
   security_group_ids = [module.app_security_group.this_security_group_id]
--- modules/variables/variables.tf
+++ modules/variables/variables.tf
@@ -1,1 +1,75 @@
 # Variable declarations
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr_block" {
+  description = "the cidr block for the vpc"
+  default     = "10.0.0.0/16"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of instances to provision"
+  type       = number
+  default     = 2
+}
+
+variable "enable_vpn_gateway" {
+  description = "Enable a VPN gateway in your VPC"
+  type        = bool
+  default     = false
+}
+
+variable "public_subnet_count" {
+  default     = 2
+  description = "Number of public subnets"
+  type        = number
+}
+
+variable "private_subnet_count" {
+  default     = 2
+  description = "Number of private subnets"
+  type        = number
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "Available cidr blocks for public subnets."
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24",
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Available cidr blocks for private subnets."
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24",
+    "10.0.105.0/24",
+    "10.0.106.0/24",
+    "10.0.107.0/24",
+    "10.0.108.0/24",
+  ]
+}
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default = {
+    project     = "learning-terraform",
+    environment = "dev"
+  }
+}
