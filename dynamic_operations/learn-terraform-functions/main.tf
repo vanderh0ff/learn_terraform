8f83d6b12420b9ec48c3f85ebeadfb0c61fb8c82
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-09-25 14:30:55 -0400

    added user template and ssh

--- dynamic_operations/learn-terraform-functions/main.tf
+++ dynamic_operations/learn-terraform-functions/main.tf
@@ -60,7 +60,25 @@ resource "aws_instance" "web" {
   ami                         = lookup(var.aws_amis, var.aws_region)
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet_public.id
-  vpc_security_group_ids      = [aws_security_group.sg_8080.id]
+  vpc_security_group_ids      = [aws_security_group.sg_8080.id, aws_security_group.sg_22.id]
   associate_public_ip_address = true
+  user_data = templatefile("user_data.tftpl", {department = var.user_department, name = var.user_name})
+  key_name = aws_key_pair.ssh_key.key_name
 }
 
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name = "ssh_key"
+  public_key = file("ssh_key.pub")  
+}
